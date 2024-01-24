e95fc06c61a8829d3e1179d506940f06fd1db203
Manju <mann.8139@gmail.com>
2024-01-24 08:45:14 +0530

    removed validation in user entity not required

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+// import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'users' })
@@ -8,18 +8,11 @@ export class User {
   id: number;
 
   @Column()
-  @IsNotEmpty({ message: 'Name cannot be empty' })
-  @IsString({ message: 'Name cannot be number' })
   name: string;
 
   @Column()
-  @IsNotEmpty({ message: 'Age column cannot be empty' })
-  @IsNumber({}, { message: 'Age should be Number' })
-  @Min(18, { message: 'age cannot be below 18' })
   age: number;
 
   @Column()
-  @IsNotEmpty({ message: 'Designation cannot be empty' })
-  @IsString({ message: 'Designation should be string' })
   designation: string;
 }
